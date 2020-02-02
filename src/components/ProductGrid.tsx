0717e665eac11acbc81f86328e82c036f6e2e355
Patrick Brandt <patrick@fullstory.com>
2020-02-02 10:12:38 -0500

    data-driven product grid

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,41 +1,51 @@
 import React from 'react';
 import {
-  Card,
   Container,
   Col,
   Row,
-  Button,
 } from 'react-bootstrap';
 import ProductCard from './ProductCard';
+import { Product } from '../services/product';
 
-const ProductGrid = () => {
-return(
-  <Container>
-    <Row>
-      <Col md={4}>
-        <ProductCard 
-          title = "Carambola" 
-          description = "Some quick example text to build on the card title and make up the bulk of the card's content."
-          price = "2.99 ea."
-          image = "https://fruitshoppe.firebaseapp.com/images/carambola.jpg" />        
-      </Col>
-      <Col md={4}>
-      <ProductCard 
-          title = "Bluebs" 
-          description = "Some quick example text to build on the card title and make up the bulk of the card's content."
-          price = "2.99 ea."
-          image = "https://fruitshoppe.firebaseapp.com/images/bluebs.jpg" />
-      </Col>
-      <Col md={4}>
-      <ProductCard 
-          title = "Dragonfruit" 
-          description = "Some quick example text to build on the card title and make up the bulk of the card's content."
-          price = "2.99 ea."
-          image = "https://fruitshoppe.firebaseapp.com/images/dragonfruit.jpg" />
-      </Col>
-    </Row>
-  </Container>
-);
+type ProductGridProps = { productList: Product[] };
+
+const COLUMN_COUNT = 3;
+
+const ProductGrid = ({ productList }: ProductGridProps) => {
+  let productMatrix: Product[][] = [];
+  let innerArray: Product[] = [];
+  let totalProductsAdded = 0;
+  productList.forEach(el => {    
+    innerArray.push(el);
+    if (innerArray.length === COLUMN_COUNT) {
+      totalProductsAdded += COLUMN_COUNT;
+      productMatrix.push(innerArray);
+      innerArray = [];
+    }
+  });
+
+  // handle any remaining elements
+  productMatrix.push(productList.slice(totalProductsAdded));
+
+  return(
+    <Container>
+      { productMatrix.map((el, i) => {
+        return (
+          <Row key={i}>
+            { el.map((product, j) => {
+              return (
+              <Col key={j} md={4}>
+                <ProductCard 
+                title = {product.title} 
+                description = {product.description}
+                price = {product.price}
+                image = {product.image} />
+              </Col>);
+            })}
+          </Row>);
+      })}
+    </Container>
+  );
 }
 
 export default ProductGrid;
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,4 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductGrid from './ProductGrid';
+import { getProducts, Product } from '../services/product';
 
-export default ProductGrid;
+const Products = () => {
+  const [productList, setProductList] = useState([] as Product[]);
+
+  useEffect(() => {
+    getProducts().then(products => setProductList(products));
+  }, []);
+
+  return(
+    <ProductGrid productList={ productList } />
+  );
+};
+
+export default Products;
