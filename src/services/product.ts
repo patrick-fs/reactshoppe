1f59f36b4f469b6b227e3f1d9174dec178bea073
Patrick Brandt <patrick@fullstory.com>
2020-02-02 08:33:08 -0500

    adding GetProducts function

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,1 +1,16 @@
-export default {};
+type Product = {
+  title: String,
+  description: String,
+  price: String,
+  image: String,
+}
+
+export const GetProducts = async () : Promise<Product[]> => {
+  const response = await fetch('data/products.json');
+  const products: Product[] = await response.json();
+  return products.map(el => {
+    return { 
+      ...el,
+      image: `https://fruitshoppe.firebaseapp.com/images/${el.image}` };
+    });
+}
