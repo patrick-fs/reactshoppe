addbae637c3b1edac1a7486e19bdbf2ebc500e9d
Patrick Brandt <patrick@fullstory.com>
2020-02-03 22:47:32 -0500

    using redux router link on brand header

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,11 +3,12 @@ import {
   Navbar,
   Nav } from 'react-bootstrap';
 import CartButton from './CartButton';
+import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
     <Navbar bg="light" expand="lg" sticky="top">
-    <Navbar.Brand href="/">Reactshoppe</Navbar.Brand>
+    <Navbar.Brand><Link to="/" className="headerLink">Reactshoppe</Link></Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
     <Nav className="mr-auto"></Nav>
