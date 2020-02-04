5d8e0411862393054734a71b0d13c24fab267aff
Patrick Brandt <patrick@fullstory.com>
2020-02-03 23:39:13 -0500

    renaming cart reducer file

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import cartReducer from './cartReducer';
+import cartReducer from './cart';
 
 const rootReducer = combineReducers({
   cartState: cartReducer,
