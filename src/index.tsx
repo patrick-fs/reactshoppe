7d8212d87d3478c43afad0c43ebb488c0226dd49
Patrick Brandt <patrick@fullstory.com>
2020-02-16 07:12:53 -0500

    adding production env test before initializing sentry + fs

--- src/index.tsx
+++ src/index.tsx
@@ -13,13 +13,15 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-FullStory.init({ orgId: 'QNEN8' });
+if (process.env.NODE_ENV !== 'production') {
+  FullStory.init({ orgId: 'QNEN8' });
 
-Sentry.init({
-  dsn: 'https://87dd10a700ea41e9a64df50ec9b367c7@sentry.io/2418505',
-  integrations: [ new SentryFullStory('sentry-test') ],
-  release: process.env.REACT_APP_SENTRY_RELEASE,
-});
+  Sentry.init({
+    dsn: 'https://87dd10a700ea41e9a64df50ec9b367c7@sentry.io/2418505',
+    integrations: [ new SentryFullStory('sentry-test') ],
+    release: process.env.REACT_APP_SENTRY_RELEASE,
+  });
+}
 
 ReactDOM.render(
   <Router>
