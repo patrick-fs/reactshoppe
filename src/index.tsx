de6249c41956f7186e6cd0c32c91337932d9fea7
Patrick Brandt <patrick@fullstory.com>
2020-02-16 09:27:05 -0500

    adding font-awesome styles

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,7 @@ import {
   BrowserRouter as Router,
 } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
