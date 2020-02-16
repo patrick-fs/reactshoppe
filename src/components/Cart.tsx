0ac7cdc3a884397a190f818219178ba6fbf9aa66
Patrick Brandt <patrick@fullstory.com>
2020-02-16 10:58:47 -0500

    adding 'Keep shoppe-ing' link

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -17,6 +17,12 @@ const Cart = () => {
     <Container>
       <Row>
         <Col>
+          <span style={{float:'right'}}><Link to="/">Keep shoppe-ing</Link></span>
+          <h4>Your Cart</h4>
+        </Col>
+      </Row>
+      <Row>        
+        <Col>          
           <CartItems cartView={ViewModifier.DELETE} />          
           <Link to="/checkout" style={{float:'right', padding:'1rem 0 0 0'}}>
             <Button variant="primary">Continue to Checkout</Button>
