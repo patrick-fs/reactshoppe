2a5ef9ca168075848ad4585b2877ba90ed72b9d8
Patrick Brandt <patrick@fullstory.com>
2020-02-17 23:02:13 -0500

    adding middleware that results in an error

---
+++ src/services/analytics.ts
@@ -0,0 +1,18 @@
+import { Product } from "../types/product";
+import * as FullStory from '@fullstory/browser';
+
+export const trackCartSize = (products: Product[]) => {
+  const totalCartRevenue = products.reduce((total, current) => {
+    return total + current.price;
+  }, 0);
+
+  let avgRevenuePerItem = totalCartRevenue / products.length;
+
+  if (isNaN(avgRevenuePerItem)) throw new Error('NaN');
+
+  const val = parseFloat(avgRevenuePerItem.toFixed(2));
+  
+  console.log(`avgRevenuePerItem: ${val}`);
+
+  FullStory.event('Revenue per cart item', { avgRevenuePerItem_real: avgRevenuePerItem });
+}
---
+++ src/store/analyticsMiddleware.ts
@@ -0,0 +1,29 @@
+import { Dispatch, Middleware, MiddlewareAPI } from 'redux'
+import { RootState } from '../store'
+import { CartActionTypes,
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+} from '../types/cart';
+import { trackCartSize } from '../services/analytics';
+import * as Sentry from '@sentry/browser';
+
+const analyticsMiddleware: Middleware<
+  {}, 
+  RootState, 
+  Dispatch
+> = (api: MiddlewareAPI<Dispatch, RootState>) => (next: Dispatch) => (action: CartActionTypes) => {
+
+  const result = next(action);
+  const { products } = api.getState().cartState;
+  try {
+    if (action.type === ADD_TO_CART || action.type === REMOVE_FROM_CART) {
+      trackCartSize(products);
+    }
+  } catch (error) {
+    Sentry.captureException(error);
+  }
+  
+  return result;
+};
+
+export default analyticsMiddleware;
