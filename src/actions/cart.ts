975285b4031794751a7b64859e6ce222af8c1609
Patrick Brandt <patrick@fullstory.com>
2020-02-27 14:21:08 -0500

    refactoring checkout form submission to use Redux flow

--- src/actions/cart.ts
+++ src/actions/cart.ts
@@ -1,5 +1,11 @@
 import { Product } from "../types/product";
-import { ADD_TO_CART, REMOVE_FROM_CART, VIEW_CART, CartActionTypes } from '../types/cart';
+import { ADD_TO_CART,
+  REMOVE_FROM_CART,
+  VIEW_CART,
+  CHECKOUT_CART,
+  CartActionTypes } from '../types/cart';
+import { CheckoutData } from '../types/checkout';
+import { makePurchase } from '../services/checkout';
 import { History } from 'history';
 
 // TODO: manage side-effects, update cart in local storage with the cart service
@@ -16,11 +22,19 @@ export const removeFromCart = (location: number) : CartActionTypes => {
     type: REMOVE_FROM_CART,
     location,
   }
-}
+};
 
 export const viewCart = (history: History) : CartActionTypes => {
   history.push('/cart');
   return {
     type: VIEW_CART,
   }
-}
+};
+
+export const checkoutCart = (history: History, checkoutData: CheckoutData) : CartActionTypes => {
+  makePurchase(checkoutData);
+  history.push('/thankyou');
+  return {
+    type: CHECKOUT_CART,
+  }
+};
