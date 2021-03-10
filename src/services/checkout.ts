1d866586cf3202543417e696dfb3083679dde96b
Patrick Brandt <patrick@fullstory.com>
2021-03-10 10:56:59 -0500

    throw error: billing address 2 is required

--- src/services/checkout.ts
+++ src/services/checkout.ts
@@ -2,7 +2,7 @@ import { CheckoutData } from '../types/checkout';
 
 const validateCheckout = (checkoutData: CheckoutData) => {
   if (checkoutData.paymentInfo.billingAddress.street2 === undefined) {
-    //throw new Error('billing address street 2 is required');
+    throw new Error('billing address street 2 is required');
   }
 };
 
