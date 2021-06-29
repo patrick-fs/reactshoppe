004fc94ba394b30feef7be9a52fe3cd8f201485d
Patrick Brandt <patrick@fullstory.com>
2021-06-29 12:45:02 -0400

    adding env vars for events endpoint and org id

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="react-scripts" />
+
+/// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+    interface ProcessEnv {
+      REACT_APP_EVENTS_ENDPOINT: string;
+      REACT_APP_ORG_ID: boolean;
+    }
+  }
